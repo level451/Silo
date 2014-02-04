70cf18e3ce07aa15c7702133963d3eb0a85983c0
twitzel <twitzel@gmail.com>
2014-02-03 17:01:53 -0700

    test

---
+++ Email.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Todd on 2/3/14.
+ */
+
+
+
+var nodemailer = require("nodemailer");
+
+// create reusable transport method (opens pool of SMTP connections)
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "twitzel@gmail.com",
+        pass: "oghjfbubihylhegz"
+    }
+});
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: "Node ✔ <foo@blurdybloop.com>", // sender address
+    to: "Steve@wizcomputing.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world ✔", // plaintext body
+    html: "<b>Hello world ✔</b>" // html body
+}
+
+// send mail with defined transport object
+smtpTransport.sendMail(mailOptions, function(error, response){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Message sent: " + response.message);
+    }
+
+    // if you don't want to use this transport object anymore, uncomment following line
+    //smtpTransport.close(); // shut down the connection pool, no more messages
+});
